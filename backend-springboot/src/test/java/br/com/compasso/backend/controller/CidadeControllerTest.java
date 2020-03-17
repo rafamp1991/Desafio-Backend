785b1cabbe5d56907dd29e24dbf684ff38f58794
Rafael Martins de Padua <rafael.padua@pampa.compasso>
2020-03-17 15:43:27 -0300

    Commit 037 - Revisando os testes

--- backend-springboot/src/test/java/br/com/compasso/backend/controller/CidadeControllerTest.java
+++ backend-springboot/src/test/java/br/com/compasso/backend/controller/CidadeControllerTest.java
@@ -2,18 +2,22 @@ package br.com.compasso.backend.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import br.com.compasso.backend.model.CidadeModel;
+import br.com.compasso.backend.model.EstadoModel;
 import br.com.compasso.backend.repository.CidadeRepository;
 import br.com.compasso.backend.repository.EstadoRepository;
 
@@ -51,16 +55,65 @@ public class CidadeControllerTest {
         cidadeModel2.setCapital(false);
     }
 	
+//	@Test
+//	public void getCidades() throws Exception {
+//		List<CidadeModel> cidadeList = new ArrayList<>();
+//		cidadeList.add(cidadeModel);
+//		cidadeList.add(cidadeModel2);
+//		
+//		Mockito.when(cidadeRepository.findAll()).thenReturn(cidadeList);
+//		
+//		mockMvc.perform(MockMvcRequestBuilders.get("/cidades").contentType(MediaType.APPLICATION_JSON))
+//		.andExpect(MockMvcResultMatchers.jsonPath("$.cidades").exists())
+//		.andExpect(MockMvcResultMatchers.jsonPath("$.cidades[*].cidadeId").isNotEmpty());
+//	}
+	
+//	@Test
+//	public void cidadeCreate() throws Exception {
+//	    Mockito.doReturn(Optional.empty())
+//		.when(cidadeRepository)
+//		.findCidadeByNome("Zacarias");
+//	    mockMvc.perform(MockMvcRequestBuilders.get("/cidade/nome"))
+//	            .andExpect(MockMvcResultMatchers.status().is(HttpStatus.NOT_FOUND.value()));
+//	}
+	
 	@Test
-	public void getCidades() throws Exception {
-		List<CidadeModel> cidadeList = new ArrayList<>();
-		cidadeList.add(cidadeModel);
-		cidadeList.add(cidadeModel2);
-		
-		Mockito.when(cidadeRepository.findAll()).thenReturn(cidadeList);
-		
-		mockMvc.perform(MockMvcRequestBuilders.get("/cidades").contentType(MediaType.APPLICATION_JSON))
-		.andExpect(MockMvcResultMatchers.jsonPath("$.cidades").exists())
-		.andExpect(MockMvcResultMatchers.jsonPath("$.cidades[*].cidadeId").isNotEmpty());
+	public void getCidadesModels() throws Exception {
+		List<CidadeModel> listaCidade = new ArrayList<CidadeModel>();
+	    Mockito.doReturn(listaCidade)
+		.when(estadoRepository)
+		.findAll();
+	    mockMvc.perform(MockMvcRequestBuilders.get("/cidades"))
+	            .andExpect(MockMvcResultMatchers.status().is(HttpStatus.NOT_FOUND.value()));
+	}
+	
+	@Test
+	public void getCidadesByEstadoUf() throws Exception {
+		EstadoModel estado = new EstadoModel();
+	    Mockito.doReturn(estado)
+		.when(estadoRepository)
+		.findByUf("AC");
+	    mockMvc.perform(MockMvcRequestBuilders.get("/cidade/estadoUf/uf"))
+	            .andExpect(MockMvcResultMatchers.status().is(HttpStatus.NOT_FOUND.value()));
+	}
+	
+	@Test
+	public void getCidadesByEstadoNome() throws Exception {
+		EstadoModel estado = new EstadoModel();
+	    Mockito.doReturn(estado)
+		.when(estadoRepository)
+		.findByNome("Rio de Janeiro");
+	    mockMvc.perform(MockMvcRequestBuilders.get("/cidade/estadoNome/nome"))
+	            .andExpect(MockMvcResultMatchers.status().is(HttpStatus.NOT_FOUND.value()));
+	}
+	
+	@Test
+	public void getByNome() throws Exception {
+		List<CidadeModel> listaCidade = new ArrayList<CidadeModel>();
+	    Mockito.doReturn(listaCidade)
+		.when(cidadeRepository)
+		.findByNome("Zacarias");
+	    mockMvc.perform(MockMvcRequestBuilders.get("/cidade/nome"))
+	            .andExpect(MockMvcResultMatchers.status().is(HttpStatus.NOT_FOUND.value()));
 	}
 }
