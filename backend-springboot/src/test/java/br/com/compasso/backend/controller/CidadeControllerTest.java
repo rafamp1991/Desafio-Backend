ac69f6ced0fc9ea932caeb5a468b4c60a1e7c37d
Rafael Martins de Padua <rafael.padua@pampa.compasso>
2020-03-18 10:05:56 -0300

    Commit 040 - Revisando os testes

--- backend-springboot/src/test/java/br/com/compasso/backend/controller/CidadeControllerTest.java
+++ backend-springboot/src/test/java/br/com/compasso/backend/controller/CidadeControllerTest.java
@@ -3,7 +3,6 @@ package br.com.compasso.backend.controller;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -14,9 +13,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import br.com.compasso.backend.model.CidadeModel;
 import br.com.compasso.backend.model.EstadoModel;
 import br.com.compasso.backend.repository.CidadeRepository;
@@ -25,9 +28,12 @@ import br.com.compasso.backend.repository.EstadoRepository;
 @WebMvcTest(CidadeController.class)
 @ActiveProfiles("test")
 public class CidadeControllerTest {
-
+ 
 	@Autowired
 	MockMvc mockMvc;
+	
+	@Autowired
+	ObjectMapper objectMapper;
 	
 	@MockBean
     private CidadeRepository cidadeRepository;
@@ -51,7 +57,7 @@ public class CidadeControllerTest {
         estadoModel = new EstadoModel();
         estadoModel.setEstadoId(35L);
         estadoModel.setNome("São Paulo");
-        estadoModel.setUf("SP");  
+        estadoModel.setUf("SP"); 
     }
 	
 	@Test
@@ -100,11 +106,61 @@ public class CidadeControllerTest {
 	            .andExpect(MockMvcResultMatchers.status().is(HttpStatus.OK.value()));
 	}
 	
-//	@Test
-//    public void cidadeCreate() throws Exception {
-//     
-//        Mockito.doReturn(Optional.of(cidadeModel)).when(cidadeRepository).findById(3557154L);
-//        mockMvc.perform(MockMvcRequestBuilders.post("/cidade"))
-//        .andExpect(MockMvcResultMatchers.status().is(HttpStatus.CREATED.value()));
-//    }	
+	@Test
+    public void cidadeCreate() throws Exception {
+		
+		cidadeModel = new CidadeModel();
+        cidadeModel.setCidadeId(1L);
+        cidadeModel.setNome("CidadeTeste");
+        cidadeModel.setLatitude(-27.0000);
+        cidadeModel.setLongitude(-50.0000);
+        cidadeModel.setCapital(true);
+		
+        mockMvc.perform(MockMvcRequestBuilders.post("/cidade")
+        		.contentType(MediaType.APPLICATION_JSON)
+	            .content(objectMapper.writeValueAsString(cidadeModel)))
+	    		.andExpect(MockMvcResultMatchers.status().is(HttpStatus.CREATED.value()));
+    }
+	
+	@Test
+    public void cidadeUpdate() throws Exception {
+		
+		CidadeModel atualizaCidade = new CidadeModel();
+		atualizaCidade.setCidadeId(3557154L);
+		atualizaCidade.setNome("CidadeTeste");
+		atualizaCidade.setLatitude(-35.0000);
+		atualizaCidade.setLongitude(-20.0000);
+		atualizaCidade.setCapital(false);
+       
+	    when(cidadeRepository.findById(3557154L)).thenReturn(Optional.of(atualizaCidade));
+	    when(cidadeRepository.save(any(CidadeModel.class))).thenReturn(atualizaCidade);
+        
+	    mockMvc.perform(MockMvcRequestBuilders.put("/cidade/3557154")
+	    		.contentType(MediaType.APPLICATION_JSON)
+	            .content(objectMapper.writeValueAsString(atualizaCidade)))
+	    		.andExpect(MockMvcResultMatchers.status().is(HttpStatus.OK.value()))
+	            .andExpect(jsonPath("$.cidadeId", is(3557154)))
+	            .andExpect(jsonPath("$.nome", is("CidadeTeste")))
+	            .andExpect(jsonPath("$.latitude", is(-35.0000)))
+	            .andExpect(jsonPath("$.longitude", is(-20.0000)))
+	            .andExpect(jsonPath("$.capital", is(false)));
+    }
+	
+	@Test
+    public void cidadeDelete() throws Exception {
+		
+		cidadeModel = new CidadeModel();
+        cidadeModel.setCidadeId(1L);
+        cidadeModel.setNome("CidadeTeste");
+        cidadeModel.setLatitude(-27.0000);
+        cidadeModel.setLongitude(-50.0000);
+        cidadeModel.setCapital(true);
+		
+        when(cidadeRepository.findById(3557154L)).thenReturn(Optional.of(cidadeModel));
+        
+        mockMvc.perform(MockMvcRequestBuilders.delete("/cidade/3557154"))
+	    		.andExpect(MockMvcResultMatchers.status().is(HttpStatus.OK.value()));
+        
+        verify(cidadeRepository, times(1)).deleteById(3557154L);
+    }
 }
