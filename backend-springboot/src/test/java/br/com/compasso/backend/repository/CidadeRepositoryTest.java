fc3d19ecf885eee02a836a061b7be69d0e16de5f
Rafael Martins de Padua <rafael.padua@pampa.compasso>
2020-03-18 17:17:38 -0300

    Revisando

--- backend-springboot/src/test/java/br/com/compasso/backend/repository/CidadeRepositoryTest.java
+++ backend-springboot/src/test/java/br/com/compasso/backend/repository/CidadeRepositoryTest.java
@@ -1,8 +1,9 @@
 package br.com.compasso.backend.repository;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
--- backend-springboot/src/test/java/br/com/compasso/backend/repository/ClienteRepositoryTest.java
+++ backend-springboot/src/test/java/br/com/compasso/backend/repository/ClienteRepositoryTest.java
@@ -1,7 +1,8 @@
 package br.com.compasso.backend.repository;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
