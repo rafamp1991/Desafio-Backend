3a8ad99ee20432413702abdb42e43815c167796b
Rafael Martins de Padua <rafael.padua@pampa.compasso>
2020-03-19 09:28:13 -0300

    Commit 048 - Revisando o projeto

--- backend-springboot/src/main/java/br/com/compasso/backend/DesafioBackendApplication.java
+++ backend-springboot/src/main/java/br/com/compasso/backend/DesafioBackendApplication.java
@@ -2,17 +2,8 @@ package br.com.compasso.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-
-//@EntityScan(basePackages = "br.com.compasso.backend.model")
-//@ComponentScan(basePackages = {"br.*"})
-//@EnableJpaRepositories(basePackages = {"br.com.compasso.backend.repository"})
-//@EnableTransactionManagement
 @EnableWebMvc
 @SpringBootApplication
 public class DesafioBackendApplication {
