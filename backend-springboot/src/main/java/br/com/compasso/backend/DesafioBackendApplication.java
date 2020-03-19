71a9486841419b7d048fb676a2e36ba83e0f56bb
Rafael Martins de Padua <rafael.padua@pampa.compasso>
2020-03-19 09:43:33 -0300

    Commit 049 - Configurando o Swagger

--- backend-springboot/src/main/java/br/com/compasso/backend/DesafioBackendApplication.java
+++ backend-springboot/src/main/java/br/com/compasso/backend/DesafioBackendApplication.java
@@ -2,13 +2,10 @@ package br.com.compasso.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
 @SpringBootApplication
 public class DesafioBackendApplication {
 
-	
 	/**
 	 * Método principal da aplicação, responsável por executar a mesma
 	 * @param args
